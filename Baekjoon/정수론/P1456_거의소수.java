5b12ef420674fbba39076800cb508215612190a0
jettieb <bokjihee@naver.com>
2025-04-21 17:42:33 +0900

    250421 백준 '거의 소수' 주석 추가

--- Baekjoon/정수론/P1456_거의소수.java
+++ Baekjoon/정수론/P1456_거의소수.java
@@ -21,11 +21,20 @@ public class P1456_거의소수 {
         int cnt = 0;
         for(int i=2; i<arr.length; i++){
             if(arr[i] == false){
+                /*
+                * i * i를 한 경우 int로 계산 후에 long으로 변환하기 때문에, 문제가 생길 수 있음!
+                * 따라서 long으로 먼저 형변환 후 계산.
+                * */
                 long temp = (long)i * i;
                 while(temp <= b){
                     if(temp >= a){
                         cnt ++;
                     }
+                    /*
+                    * i의 최댓값이 10^7이기 때문에, temp *= i를 했을 때 오버플로우가 나서
+                    * 이상한 숫자로.. while문에 들어가는 경우가 있을 수 있음.
+                    * 이를 방지하기 위해 오버플로우 전에 if 문으로 정지시키기.
+                    * */
                     if(temp > b / i) break;
                     temp *= i;
                 }
